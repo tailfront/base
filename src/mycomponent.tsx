09e03d0bd52341dc143ec046bad098bea27d1ff7
khasandesign <khasan.shadiyarov@gmail.com>
2024-07-13 16:27:09 +0500

    2024-07-13

---
+++ src/mycomponent.tsx
@@ -0,0 +1,38 @@
+/**
+ * @file Copybox is a wrapper to copy associated value to clipboard.
+ * @example
+ * ```tsx
+ * import {Copybox} from '@/components/copybox';
+ *
+ * <Copybox value="abc123"><span>ABC123</span></Copybox>
+ * ```
+ */
+
+import React from 'react';
+import {toClipboard} from '@/lib/utils';
+import {toast} from '@/components/use-toast';
+
+interface CopyboxProps {
+  value: string;
+  children: React.ReactElement;
+}
+
+const Copybox = ({value, children}: CopyboxProps): JSX.Element => {
+  return (
+    <span
+      className="cursor-pointer"
+      onClick={() => {
+        toClipboard(value);
+        toast({
+          title: 'Скопировано в буфер обмена',
+          description: value,
+        });
+      }}
+    >
+      {children}
+    </span>
+  );
+};
+
+export type {CopyboxProps};
+export {Copybox};
