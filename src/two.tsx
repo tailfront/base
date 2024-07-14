dc1b11590c6a44c2b1e02f529401906d9ed51159
khasandesign <khasan.shadiyarov@gmail.com>
2024-07-14 17:47:11 +0500

    2024-07-14

---
+++ src/two.tsx
@@ -0,0 +1,15 @@
+/**
+ * @file Copybox is a wrapper to copy associated value to clipboard.
+ * @example
+ * ```tsx
+ * import {Copybox} from '@/components/copybox';
+ *
+ * <Copybox value="abc123"><span>ABC123</span></Copybox>
+ * ```
+ */
+
+const Copybox2 = (): string => {
+  return 'Hi';
+};
+
+export {Copybox2};
