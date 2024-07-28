b4ca7c3fa12d69180e7ddde7ea0d49a032d82e1a
khasandesign <khasan.shadiyarov@gmail.com>
2024-07-29 02:40:19 +0500

    2024-07-28

--- src/button.tsx
+++ src/button.tsx
@@ -15,7 +15,7 @@
  * @require --save-dev @types/react
  * @require class-variance-authority
  * @require @radix-ui/react-slot
- * @require clsx@2.0.0
+ * @require clsx
  * @require tailwind-merge
  */
 
